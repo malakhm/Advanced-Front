7c2140ad24a4000dc8eaae0166b22ebf9d90d956
Mohamadalyouness <mohamadalyouness7@gmail.com>
2023-11-07 09:20:18 +0200

    d

--- front-end/src/pages/home/Home.js
+++ front-end/src/pages/home/Home.js
@@ -1,17 +1,54 @@
-// import "./Home.css";
+
 import React from 'react';
-import Header  from '../../components/header/Header.js';
+import Header from '../../components/header/Header.js';
 import imageHeader from '../../Photos/header-1.png';
 import HomeCategoriescard from '../../components/homeCategoriesCard/HomeCategoriesCard.js';
-import  './home.css';
+import './home.css';
 const Home = () => {
     return (
 
         <div className="home">
-           <Header imageSrc={imageHeader} />
-           <HomeCategoriescard/>
+            <Header imageSrc={imageHeader} />
+            <HomeCategoriescard />
+
+            <section className='home-comments'>
+                <section className='comments-left'>
+                    <p className='review'>review</p>
+                    <p className='comment-description'><strong> Over 100 designs completed for happy clients.</strong></p>
+                </section>
+
+                <section className='comments-right'>
+                    <section className='comments-item'>
+                        <h1 className='name-comments'>sara_designer</h1>
+                        <p className='comment-itself'>The use of color in this design is amazing! I'm getting so much inspiration.</p>
+                    </section>
+                    <section className='comments-item'>
+                        <h1 className='name-comments'>sara_designer</h1>
+                        <p className='comment-itself'>The use of color in this design is amazing! I'm getting so much inspiration.</p>
+                    </section>
+                    <section className='comments-item'>
+                        <h1 className='name-comments'>sara_designer</h1>
+                        <p className='comment-itself'>The use of color in this design is amazing! I'm getting so much inspiration.</p>
+                    </section>
+                    <section className='comments-item'>
+                        <h1 className='name-comments'>sara_designer</h1>
+                        <p className='comment-itself'>The use of color in this design is amazing! I'm getting so much inspiration.</p>
+                    </section>
+                    <section className='comments-item'>
+                        <h1 className='name-comments'>sara_designer</h1>
+                        <p className='comment-itself'>The use of fdpfksfosfdk  fssfkfpsd scolor in this design is amazing! I'm getting so much inspiration.</p>
+                    </section>
+
+                    <section className='comments-item'>
+                        <h1 className='name-comments'>sara_designer</h1>
+                        <p className='comment-itself'>The use of color in this design is amazing! I'm getting so much inspiration.</p>
+
+                    </section>
+
+                </section>
+            </section>
         </div>
-        
+
     )
 }
 

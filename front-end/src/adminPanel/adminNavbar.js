abdc2fba1b105c82cecc984b7a53dd352b325746
alikansoh <alikansoh.dev@gmail.com>
2023-11-08 12:05:52 +0200

    added same pages

---
+++ front-end/src/adminPanel/adminNavbar.js
@@ -0,0 +1,26 @@
+// Admin/AdminNavbar.js
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../Photos/Logo.png'
+
+
+function AdminNavbar() {
+
+  return (
+  <header className='header'>
+   <img src={Logo} alt='' className='Logo' />
+    <div className="navbar">
+     <div className="header_Links">
+            <Link to="/admin" className="">Companies</Link>
+            <Link to="/admin/Categories" className="">Categories</Link>
+            <Link to="/admin/comments" className="">Comments</Link>
+
+            
+      </div>
+    </div>
+    </header>
+  );
+}
+
+export default AdminNavbar;

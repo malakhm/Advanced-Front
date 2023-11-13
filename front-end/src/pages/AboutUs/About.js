64273826491f8abafe78c6e5d6ab96a59960a98f
Mohamadalyouness <mohamadalyouness7@gmail.com>
2023-11-13 13:13:29 +0200

    new

--- front-end/src/pages/AboutUs/About.js
+++ front-end/src/pages/AboutUs/About.js
@@ -24,7 +24,7 @@ const AboutPage = () => {
     };
 
     try {
-      const response = await fetch("/https://spaceloom.onrender.com/api/feedbacks/", {
+      const response = await fetch("/api/feedbacks/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",

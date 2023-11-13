3cf49585595b7a6bd2b9ec2296e79effe71e2645
Mohamadalyouness <mohamadalyouness7@gmail.com>
2023-11-13 09:27:36 +0200

    new

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { disablereactdevtools } from '@fvilers/disable-react-devtools';
-
-if (Process.env.NODE_ENV !== 'production') disablereactdevtools ();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

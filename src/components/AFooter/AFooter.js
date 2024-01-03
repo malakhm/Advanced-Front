6f0c1b4aa02c833c67d81d1884c819e96a478ec5
anitta-sleiman <sleiman3198@gmail.com>
2024-01-03 18:30:26 +0200

    added contact us form/page

---
+++ src/components/AFooter/AFooter.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './AFooter.css'; 
+import Logo from '../../Photos/Logo.png';
+import FaceBook from '../../Photos/facebook.png';
+import whatsUp from '../../Photos/whats.png';
+import instagram from '../../Photos/instagram.png';
+import Youtube from '../../Photos/Youtube.png';
+
+
+const AFooter = () => {
+  return (
+    <footer className='footer'>
+      <div className="footer-img">
+        <img src={Logo} alt="spaceloom" />
+      </div>
+      <div className="footerContainer">
+        <h1 className='h1'>Subscribe</h1>
+        <p className='p'>Subscribe to our newsletter and stay inspired with the latest interior design trends.</p>
+        <form className='form'>
+          <input className="email" id="input" type="email" name="email" placeholder="Email address" />
+          <input className="inp" type="submit" name="submit" value="Sign up" />
+        </form>
+      </div>
+      <div className="footer-imgs">
+        <a href="https://www.fb.com">
+          <img src={FaceBook} alt="tiktokIcon" className="footer-icons" />
+        </a>
+        <a href="https://www.whatsup.com">
+          <img src={whatsUp} alt="twitterIcon" className="footer-icons" />
+        </a>
+        <a href="https://www.instagram.com">
+          <img src={instagram} alt="instagramIcon" className="footer-icons" />
+        </a>
+        <a href="https://www.youtube.com">
+          <img src={Youtube} alt="facebookIcon" className="footer-icons" />
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default AFooter;
--- src/pages/ContactUs/ContactUs.js
+++ src/pages/ContactUs/ContactUs.js
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
+import { toast} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import emailjs from "emailjs-com";
-import Navbar from "../../components/Navbar/Navbar.js"
+import ANavbar from "../../components/ANavbar/ANavbar.js";
+import AFooter from "../../components/AFooter/AFooter.js";
+// import CouchImage from "../../Photos/royal-blue-couch-living-room.jpg";
+import "./ContactUs.css";
+
 const EmailForm = () => {
   const [formData, setFormData] = useState({
     from_name: "",
@@ -9,6 +15,8 @@ const EmailForm = () => {
     message: "",
   });
   const [isMessageSent, setMessageSent] = useState(false);
+  let maxCharacters = 200;
+  const charactersLeft = maxCharacters - formData.message.length;
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -22,9 +30,15 @@ const EmailForm = () => {
     e.preventDefault();
 
     emailjs
-      .send("service_spaceloom312024", "template_sp2fvvv", formData, "zlC1TeyRq0BY2dzYp")
+      .send(
+        "service_spaceloom312024",
+        "template_sp2fvvv",
+        formData,
+        "zlC1TeyRq0BY2dzYp"
+      )
       .then((response) => {
         console.log("Email sent successfully:", response);
+        toast.success("Email sent successfully!");
         setMessageSent(true);
         setFormData({
           from_name: "",
@@ -40,51 +54,73 @@ const EmailForm = () => {
 
   return (
     <div>
-        <Navbar/>
-      <h2>Contact Form</h2>
-      <form onSubmit={sendEmail}>
-        <label>
-          Name:
-          <input
-            type="text"
-            name="from_name"
-            value={formData.from_name}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Email:
-          <input
-            type="email"
-            name="from_email"
-            value={formData.from_email}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Subject:
-          <input
-            type="text"
-            name="subject"
-            value={formData.subject}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Message:
-          <textarea
-            name="message"
-            value={formData.message}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <button type="submit">Send Email</button>
-      </form>
-      {isMessageSent && <p>Email sent successfully!</p>}
+      <ANavbar />
+      <h3 className="contactus-title">Send us a message now!</h3>
+      <div className="contactus-container container">
+        <div className="contact-box">
+          <form className="contactus-form right" onSubmit={sendEmail}>
+            <label>
+              Name
+              <input
+                type="text"
+                name="from_name"
+                value={formData.from_name}
+                onChange={handleInputChange}
+                required
+                className="field"
+              />
+            </label>
+
+            <label>
+              Email
+              <input
+                type="email"
+                name="from_email"
+                value={formData.from_email}
+                onChange={handleInputChange}
+                required
+                className="field"
+              />
+            </label>
+
+            <label>
+              Subject
+              <input
+                type="text"
+                name="subject"
+                value={formData.subject}
+                onChange={handleInputChange}
+                required
+                className="field"
+              />
+            </label>
+
+            <label className="message-contact">
+              Message
+              <textarea
+                name="message"
+                value={formData.message}
+                onChange={handleInputChange}
+                required
+                maxLength={maxCharacters}
+                className="field"
+              />
+            </label>
+            <span className="character-count">
+              {charactersLeft} characters left
+            </span>
+            <button className="btn btn-blue" type="submit">
+              Send Email
+            </button>
+          </form>
+          {isMessageSent}
+          <div className="image-container-contactus left">
+            {/* <img src={CouchImage} alt="background-contactus" /> */}
+          </div>
+        </div>
+      </div>
+
+      <AFooter />
     </div>
   );
 };
